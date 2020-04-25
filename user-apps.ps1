3be3e97fbb44fa0b3ad6e3a247cb4ec8f8484576
Florent Roques <florent.open@gmail.com>
2020-04-25 12:23:16 +0200

    add needed functions
    TODO: check if functions already exist

--- user-apps.ps1
+++ user-apps.ps1
@@ -1,3 +1,11 @@
+function Verify-Elevated {
+    # Get the ID and security principal of the current user account
+    $myIdentity=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myPrincipal=new-object System.Security.Principal.WindowsPrincipal($myIdentity)
+    # Check to see if we are currently running "as Administrator"
+    return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
 # Check to see if we are currently running "as Administrator"
 if (!(Verify-Elevated)) {
    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
@@ -6,6 +14,23 @@ if (!(Verify-Elevated)) {
    [System.Diagnostics.Process]::Start($newProcess);
 
    exit
+}
+
+# Reload the $env object from the registry
+function Refresh-Environment {
+    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+                 'HKCU:\Environment'
+
+    $locations | ForEach-Object {
+        $k = Get-Item $_
+        $k.GetValueNames() | ForEach-Object {
+            $name  = $_
+            $value = $k.GetValue($_)
+            Set-Item -Path Env:\$name -Value $value
+        }
+    }
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
 ### Chocolatey
